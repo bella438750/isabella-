dfcc41a8314b4828dd23743b29e5826d3b79cde1
bella438750 <168583429+bella438750@users.noreply.github.com>
2024-11-27 07:50:36 -0300

    1

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'pergunta geral
+    'De onde é a invenção do chuveiro elétrico
+    'Quais o menor e maior país do mundo
+)
+
+criaCartao(
+    'Conhecimentos geral
+    'Qual a montanha mais alta do mundo?
+    'Que país tem o formato de uma bota?
+)
+
+criaCartao(
+    'ingles 
+    'Como se diz cachorro em ingles?
+    'Como se diz carro em ingles?
+)
+
+criaCartao(
+    'conhecimentos gerais
+    'Quem pintou guernica?
+    'Em que ordem surgiram os modelos atomicos ?
+)
